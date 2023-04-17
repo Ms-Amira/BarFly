302663f15e96b1b93f10191430227456cc01669d
Mimi <trish21215@yahoo.com>
2023-04-17 10:05:52 -0400

    create view

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
   path('accounts/signup/', views.signup, name='signup'),
-    
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('bars/', views.cats_index, name='index'),
+  path('bars/<int:bar_id>/', views.bars_detail, name='detail'),
+  path('bars/create/', views.BarCreate.as_view(), name='bars_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-
+from django.views.generic.edit import CreateView
+from .models import Bar
 from botocore.exceptions import ClientError
 import uuid
 import boto3
@@ -9,3 +10,34 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def bars_index(request):
+    bars = Bar.objects.filter(user=request.user)
+    return render(request, 'bars/index.html', {'bars': bars})
+
+class BarCreate(CreateView):
+    model = Bar
+    fields = ['name', 'address', 'theme', 'site_traffic', 'has_cover']
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
