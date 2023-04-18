82c5016138abf58ec3de606593adc5a7bcf76079
samuel-w-berg <samuel.w.berg@gmail.com>
2023-04-18 10:54:08 -0500

    Add Review Functionality

--- main_app/forms.py
+++ main_app/forms.py
@@ -5,7 +5,3 @@ class ReviewForm(ModelForm):
     class Meta:
         model = Review
         fields =['date', 'rating', 'comment']
-
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        return super().form_valid(form)
